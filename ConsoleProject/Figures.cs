22e10223ca156bcd29503b4d7c5aa5c098b57638
vande <vande@THOM>
2025-11-13 15:08:55 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleProject/Figures.cs
@@ -0,0 +1,259 @@
+﻿​
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+
+namespace nsFigures
+
+{
+
+    //=========================
+
+    internal class clsFigures
+
+    {
+
+        #region Constructeurs
+
+        //-------------------------
+
+        internal clsFigures(ushort AX, ushort AY, string ANom = "")
+
+        {
+
+            X = AX;
+
+            Y = AY;
+
+            Angle = 0.0f;
+
+            Nom = ANom ?? string.Empty;
+
+        }
+
+        #endregion
+
+
+
+        #region propriété X
+
+        // Accesseur
+
+        internal const ushort MAX_X = 800;
+
+        public ushort _X;
+
+        public ushort X // 0 à 800 pixels
+
+        {
+
+            get
+
+            {
+
+                return _X;
+
+            }
+
+            set
+
+            {
+
+                _X = (value > MAX_X) ? MAX_X : value;
+
+            }
+
+        }
+
+        #endregion
+
+
+
+        #region propriété Y
+
+        //--- Propriété Y
+
+        public const ushort MAX_Y = 480;
+
+        private ushort _Y; // Propriété privée qui contient la valeur (0 à 480 pixels)
+
+        public ushort Y // Accesseurs R/W
+
+        {
+
+            get { return _Y; } // Retour directement la valeur
+
+            set { _Y = (value > MAX_Y) ? MAX_Y : value; } // Test avec plafonnement si besoin
+
+        }
+
+        #endregion
+
+
+
+        #region Angle
+
+        //--- Propriété Angle
+
+        public const float MAX_ANGLE = 360.0f;
+
+        private float _Angle; // Propriété privée qui contient la valeur (0.0 à 360.0°)
+
+        public float Angle // Accesseurs R/W
+
+        {
+
+            get { return _Angle; } // Retour directement la valeur
+
+            set { _Angle = (value > MAX_ANGLE) ? MAX_ANGLE : (value < 0.0f ? 0.0f : value); } // Test avec plafonnement si besoin
+
+        }
+
+        #endregion
+
+
+
+        public string Nom;
+
+    }//class clsFigures
+
+
+
+    //=========================
+
+    internal class clsRectangle : clsFigures
+
+    {
+
+        internal clsRectangle(ushort AX, ushort AY, ushort ALargeur, ushort AHauteur, string ANom = "")
+
+          : base(AX, AY, ANom)
+
+        {
+
+            Largeur = ALargeur;
+
+            Hauteur = AHauteur;
+
+        }
+
+
+
+        #region propriété Largeur
+
+        //--- Propriété Y
+
+        private ushort _Largeur;
+
+        public ushort Largeur // Accesseurs R/W
+
+        {
+
+            get { return _Largeur; } // Retour directement la valeur
+
+            set { _Largeur = (value > MAX_X) ? MAX_X : value; } // Test avec plafonnement si besoin
+
+        }
+
+        #endregion
+
+
+
+        #region propriété Hauteur
+
+        //--- Propriété Hauteur
+
+        private ushort _Hauteur;
+
+        public ushort Hauteur // Accesseurs R/W
+
+        {
+
+            get { return _Hauteur; } // Retour directement la valeur
+
+            set { _Hauteur = (value > MAX_Y) ? MAX_Y : value; } // Test avec plafonnement si besoin
+
+        }
+
+        #endregion
+
+    }//class clsRectangle
+
+
+    internal class clsCarre : clsFigures
+    {
+        internal clsCarre(ushort AX, ushort AY, string ANom, ushort ALargeurHauteur)
+            : base(AX, AY, ANom)
+        {
+
+        }
+        public ushort _LargeurHauteur;
+
+        public ushort ALargeurHauteur
+        {
+            get { return _LargeurHauteur; }
+            set { _LargeurHauteur = value; }
+
+        }
+    }
+
+    internal class clsCube : clsFigures
+    {
+        internal clsCube(ushort AX, ushort AY, string ANom, float profondeur)
+            : base(AX, AY, ANom)
+        {
+            profondeur = _profondeur;
+        }
+        public float _profondeur;
+
+        public float profondeur
+        {
+            get { return _profondeur; }
+            set { _profondeur = value;
+            }
+        }
+        internal class clsCercle : clsFigures
+        {
+            internal clsCercle(ushort AX, ushort AY, string ANom, float ARayon)
+                : base(AX, AY, ANom)
+            {
+                Rayon = ARayon;
+
+            }
+            public float _Rayon;
+
+            public float Rayon
+            {
+                get { return _Rayon; }
+                set { _Rayon = value; }
+            }
+        }
+
+        internal class clsCylindre : clsCercle
+        {
+            internal clsCylindre(ushort AX, ushort AY, string ANom, float ARayon, float profondeur)
+            : base(AX, AY, ANom, ARayon)
+            {
+                profondeur = _profondeur;
+            }
+
+            public float _profondeur;
+
+            public float profondeur
+            {
+                get { return _profondeur; }
+                set { _profondeur = value; }
+            }
+        }
+
+    }
+}
+
---
+++ ConsoleProject/Program.cs
@@ -0,0 +1,225 @@
+﻿using System.Runtime.CompilerServices;
+using nsFigures;
+ 
+namespace ConsoleProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool majeur = false;
+
+            //Console.WriteLine("Quel age as tu ? ");
+            //int age = int.Parse(Console.ReadLine());
+
+
+            //if(age > 17)
+            //{
+            //    majeur = true;
+
+            //}
+
+            //Console.WriteLine("Vous êtes majeur : " + majeur);
+            //Console.ReadLine();
+
+
+            //float vitesse = 42.5F;
+            //double temperature = 32.3;
+
+            //decimal toto = 23.8M;
+
+
+            //Console.WriteLine("toto est de type : " + toto.GetType());
+
+
+            //string chaine = "ABC";
+            //char car = chaine[0];
+            //Console.WriteLine(chaine[0]);
+            //Console.WriteLine(chaine[1]);
+            //Console.WriteLine(chaine[2]);
+            //Console.WriteLine(chaine[3]);
+
+            //int i = 12;
+
+            //Console.WriteLine(i.ToString());
+            //string s = "ABC" + 12.ToString() + "_" + 1.23f.ToString() + true;
+
+            //int x = 123;
+            //byte b = 2;
+            //s = "add(" + x + "+" + b + ")=" + (x + b);
+            //Console.WriteLine(s);
+
+            //s = string.Format("add({0}+{1})={2)", i, b, i + b);
+            //Console.WriteLine(s);
+
+            //s = $"add({x}+{b})={x+b}";
+            //Console.WriteLine(s);
+
+
+            //int nombre = 127;
+
+            //string rep = $"nbr={nombre:x8}";
+            //Console.WriteLine(rep);
+
+
+            //DateTime dateTime = DateTime.Now;
+            //s = $"{dateTime:gg}";
+            //s = $"{dateTime:}";
+
+
+            //Console.WriteLine(s);
+
+            //float f = 123.33f;
+
+            //int j = 12;
+
+            //float k = (float)f;
+
+
+            //int test = int.TryParse()
+
+            //Console.WriteLine("Saisir un jour : entre 1 et 7 ");
+            //string njour = Console.ReadLine();
+            //int intJour = int.Parse(njour);
+
+            //Console.WriteLine($"{intJour}");
+
+            //switch (intJour)
+            //{
+            //    case 1:
+            //        rep = "vous êtes un Lundi";
+            //        break;
+            //    case 2:
+            //        rep = "vous êtes un Mardi";
+            //        break;
+            //    case 3:
+            //        rep = "vous êtes un Mercredi";
+            //        break;
+
+
+            //}
+
+            //while (true)
+            //{
+
+            //    Console.WriteLine(">");
+            //    string strRep = Console.ReadLine();
+
+            //    strRep = strRep.Trim();
+
+            //    if (s == "")
+            //        continue;
+
+            //    switch(s.ToLower())
+            //    {
+            //        case "*":
+            //            Formation();
+            //            break;
+
+            //        case "q":
+            //            return;
+
+            //        default:
+            //            Console.WriteLine("Erreur !!!");
+            //            break;
+
+            //    }
+            //}
+            Formation();
+
+            //int var1 = 6;
+            //int var2 = 8;
+
+            //swap(var1, var2); 
+
+       
+        }
+
+        struct Personne
+        {
+
+          public  string Nom;
+
+          public  byte Age;
+          public  float Taille;
+
+            public Adresse Lieu;
+
+        };
+
+        struct Adresse
+        {
+            public string adresse1;
+            public string adresse2;
+            public uint codePostal;
+            public string Ville;
+            public byte Etage;
+        }
+        static void valeur(int val)
+        {
+            Console.WriteLine($"Valeur={val}");
+            val = 0;
+            Console.WriteLine($"Valeur={val}");
+        }
+
+
+        public static void Formation()
+        {
+            //Personne individu;
+            //individu.Nom = "Gilles";
+            //individu.Age = 24;
+            //individu.Taille = 1.63f;
+
+            //individu.Lieu.adresse1 = "33 rue challs";
+
+            //int data = 12;
+            //valeur(data);
+            //Console.WriteLine($"Valeur={data}"); 
+
+            int i;
+            // var fig = new clsFigures(); 
+            // clsFigures fig = new clsFigures(); 
+ 
+            //fig.X = 2;
+            //fig.Y = 90;
+            //fig.Nom = "carré";
+
+
+            //clsFigures fig2 = new()
+            //{
+            //    X = 2,
+            //    Y = 3,
+            //    Nom = "",
+            //    Angle = 0.0f
+
+            //};
+
+            clsFigures fig2 = new clsFigures(200, 200, "carre");
+            ClsRectangle rectangle = new ClsRectangle(1, 2, 200, 500, "rectangle");
+
+
+            clsFigures fig3 = new ClsRectangle(900, 20, 1, 2, "recta"); 
+
+            
+        }
+        public static void swap(ref int var1,ref int var2)
+        {
+            int tempVar = var1;
+            var1 = var2;
+            var2 = var1;
+            Console.WriteLine($"Après swap : var1 = {var1}, var2 = {var2}");
+        }
+        int i = 13; 
+
+     
+    }
+
+    public static class ClsExtension
+    {
+        public static bool EstPair(this int AValue)
+        {
+            return (AValue % 2 == 0) ? true : false; 
+        }
+
+    }
+}
