cd50348de04c3481c0024c6d7ab386ac589e7a32
ThomasVDB <vandenbergthomas@hotmail.fr>
2025-11-15 14:50:36 +0100

    Fin inplémentation des formes, faut encore testeravec program.cs

--- ConsoleProject/Figures.cs
+++ ConsoleProject/Figures.cs
@@ -113,10 +113,61 @@ namespace nsFigures
         #endregion
 
 
+        #region --- Propriété Nom
 
-        public string Nom;
+        //X159#3
 
-        abstract internal void Dessine(); 
+        public string _Nom;   // Propriété privée qui contient le Nom de la figure
+
+        public string Nom // Accesseurs Read Only
+
+        {
+
+            get { return _Nom ?? ""; } // Retour directement la valeur
+            set {  _Nom = value; }
+
+        }
+
+        #endregion
+
+
+        //#region --- Propriété SupportDessin
+
+        //// static: une seule variable pour tous les objets créés (qqes soit le nombre d’objets)
+
+        //static protected nsSupportDessin.ISupportDessin? _SupportDessin; // Propriété  qui contient le support pour dessiner
+
+        //static public nsSupportDessin.ISupportDessin? SupportDessin// Accesseurs W
+
+        //{
+
+        //    set { _SupportDessin = value; }
+
+        //}
+
+        // #endregion
+
+        abstract internal void Dessine();
+
+        virtual internal void Zoom(float ACoeffX, float ACoeffY = 1.0f)
+
+        {
+
+            // Méthode par défaut : rien à traiter dans classe clsFigure !
+
+            Console.WriteLine($"clsFigure.Zoom(CoeffX={ACoeffX} CoeffX={ACoeffY} \"{Nom}\")");
+
+            Dessine();// Redessine figure
+
+        }
+
+        public override string ToString()
+
+        {
+
+            return $"Figure \"{Nom}\": X={X} Y={Y}";
+
+        }
 
     }//class clsFigures
 
@@ -140,7 +191,7 @@ namespace nsFigures
 
         }
 
-        
+
 
         #region propriété Largeur
 
@@ -185,6 +236,46 @@ namespace nsFigures
             //            Console.WriteLine($"    (Angle={Angle:0.0} C={Couleur})");
         }
 
+        override internal void Zoom(float ACoeffX, float ACoeffY = 1.0f)
+
+        {
+
+            // Expansion Largeur & Hauteur
+
+            Console.WriteLine($"clsRectangle.Zoom(CoeffX={ACoeffX} CoeffX={ACoeffY} \"{Nom}\")");
+
+
+
+            if (ACoeffX < 0.0f)
+
+                ACoeffX = 1.0f; // Coeff négatif non accepté
+
+
+
+            if (ACoeffY < 0.0f)
+
+                ACoeffY = 1.0f; // Coeff négatif non accepté
+
+
+
+            Largeur = (ushort)(Largeur * ACoeffX); // Calcul nouvelle Largeur
+
+            Hauteur = (ushort)(Hauteur * ACoeffY); // Calcul nouvelle Hauteur
+
+
+
+            Dessine();// Redessine figure
+
+        }
+
+        public override string ToString()
+
+        {
+
+            return $"Rectangle \"{Nom}\": X={X} Y={Y} L={Largeur} H={Hauteur}";
+
+        }
+
         #endregion
 
     }//class clsRectangle
@@ -208,28 +299,49 @@ namespace nsFigures
 
         internal override void Dessine()
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"--- clsRectangle.Dessine(X={X} Y={Y} C={LargeurHauteur} \"{Nom}\")");
+        }
+
+        public override string ToString()
+
+        {
+
+            return $"Carré \"{Nom}\": X={X} Y={Y} Coté={LargeurHauteur}";
+
         }
     }
-    
+
     internal class clsLigne : clsRectangle
     {
-        internal clsLigne(ushort AX, ushort AY, string ANom,ushort  ALargeur, ushort AHauteur )
-            :base(AX , AY, ALargeur, AHauteur)
+        internal clsLigne(ushort AX, ushort AY, string ANom, ushort ALargeur, ushort AHauteur)
+            : base(AX, AY, ALargeur, AHauteur)
         {
-            AHauteur = 1; 
+            AHauteur = 1;
+        }
+
+        internal override void Dessine()
+        {
+            Console.WriteLine($"-- clsRectangle.Dessine(X={X} Y={Y} C={Largeur} H={1}");
+        }
+
+        public override string ToString()
+
+        {
+
+            return $"Ligne \"{Nom}\": X={X} Y={Y} L={Largeur}";
+
         }
     }
     internal class clsCube : clsFigures
     {
-        internal clsCube(ushort AX, ushort AY, string ANom, float AProfondeur)
+        internal clsCube(ushort AX, ushort AY, string ANom, ushort AProfondeur)
             : base(AX, AY, ANom)
         {
             profondeur = AProfondeur;
         }
-        public float _profondeur;
+        public ushort _profondeur;
 
-        public float profondeur
+        public ushort profondeur
         {
             get { return _profondeur; }
             set
@@ -237,49 +349,246 @@ namespace nsFigures
                 _profondeur = value;
             }
         }
-        internal class clsCercle : clsFigures
+
+        internal override void Dessine()
         {
-            internal clsCercle(ushort AX, ushort AY, string ANom, float ARayon)
-                : base(AX, AY, ANom)
-            {
-                Rayon = ARayon;
-
-            }
-            public float _Rayon;
-
-            public float Rayon
-            {
-                get { return _Rayon; }
-                set { _Rayon = value; }
-            }
-
-            internal override void Dessine()
-            {
-                throw new NotImplementedException();
-            }
+            Console.WriteLine($"-- clsRectangle.Dessine(X={X} Y={Y} P={profondeur} ");
         }
 
-        internal class clsCylindre : clsCercle
+        internal override void Zoom(float ACoeffX, float ACoeffY = 1)
         {
-            internal clsCylindre(ushort AX, ushort AY, string ANom, float ARayon, float AProfondeur)
-            : base(AX, AY, ANom, ARayon)
-            {
-                profondeur = AProfondeur;
-            }
+            Console.WriteLine($"-- clsCube.Zoom(CoeffX ={ACoeffX} CoeffY={ACoeffY} \"{Nom} ) ");
 
-            public float _profondeur;
+            if (ACoeffX < 0.0f)
 
-            public float profondeur
-            {
-                get { return _profondeur; }
-                set { _profondeur = value; }
-            }
+                ACoeffX = 1.0f; // Coeff négatif non accepté
+
+
+            if (ACoeffY < 0.0f)
+
+                ACoeffY = 1.0f; // Coeff négatif non accepté
+
+
+            profondeur = (ushort)(profondeur * ACoeffX);
+
+            Dessine();
+        }
+
+        public override string ToString()
+
+        {
+
+            return $"Ligne \"{Nom}\": X={X} Y={Y} L={profondeur}";
+
+        }
+
+
+    }
+    internal class clsCercle : clsFigures
+    {
+        internal clsCercle(ushort AX, ushort AY, string ANom, ushort ARayon)
+            : base(AX, AY, ANom)
+        {
+            Rayon = ARayon;
+
+        }
+        public ushort _Rayon;
+
+        public ushort Rayon
+        {
+            get { return _Rayon; }
+            set { _Rayon = value; }
         }
 
         internal override void Dessine()
         {
-            throw new NotImplementedException();
+
+            Console.WriteLine($"--- clsCercle.Dessine(X={X} Y={Y} R={Rayon} \"{Nom}\")");
+
+            //Console.WriteLine($"    (C={Couleur})");
+
+        }
+
+        override internal void Zoom(float ACoeffX, float ACoeffY = 1.0f)
+
+        {
+
+            // Expansion Largeur & Hauteur
+
+            Console.WriteLine($"clsCercle.Zoom(CoeffX={ACoeffX} CoeffX={ACoeffY} \"{Nom}\")");
+
+
+
+            if (ACoeffX < 0.0f)
+
+                ACoeffX = 1.0f; // Coeff négatif non accepté
+
+
+
+            Rayon = (ushort)(Rayon * ACoeffX); // Calcul nouvelle Rayon
+
+
+
+            Dessine();// Redessine figure
+
+        }
+
+        public override string ToString()
+
+        {
+
+            return $"Cercle \"{Nom}\": X={X} Y={Y} R={Rayon}";
+
         }
     }
-}
+    internal class clsCylindre : clsCercle
+    {
+        internal clsCylindre(ushort AX, ushort AY, string ANom, ushort ARayon, ushort AProfondeur)
+        : base(AX, AY, ANom, ARayon)
+        {
+            profondeur = AProfondeur;
+        }
 
+        public ushort _profondeur;
+
+        public ushort profondeur
+        {
+            get { return _profondeur; }
+            set { _profondeur = value; }
+        }
+
+
+        internal override void Dessine()
+        {
+
+            Console.WriteLine($"--- clsCylindre.Dessine(X={X} Y={Y} R={Rayon} P={profondeur}\"{Nom}\")");
+        }
+
+        internal override void Zoom(float ACoeffX, float ACoeffY = 1)
+        {
+            // Expansion Largeur & Hauteur
+
+            Console.WriteLine($"clsCercle.Zoom(CoeffX={ACoeffX} CoeffX={ACoeffY} \"{Nom}\")");
+
+
+
+            if (ACoeffX < 0.0f)
+
+                ACoeffX = 1.0f; // Coeff négatif non accepté
+
+
+
+            profondeur = (ushort)(profondeur * ACoeffX); // Calcul nouvelle Rayon
+
+        }
+
+        public override string ToString()
+
+        {
+
+            return $"Cercle \"{Nom}\": X={X} Y={Y} R={Rayon} P={profondeur}";
+
+        }
+
+    }
+
+    internal class clsPoint : clsFigures
+
+    {
+
+        #region --- ctor
+
+        internal clsPoint(ushort AX, ushort AY, string ANom = "")
+
+          : base(AX, AY, ANom) // Appel classe Parent (ici = clsFigure)
+
+        {
+
+            // Init des propriétés de la classe clsFigure -> base(...)
+
+            Console.WriteLine($"clsPoint.ctor(X={X} Y={Y} \"{Nom}\")");
+
+        }
+
+        #endregion
+
+
+
+        #region --- Propriété SupportDessin
+
+        // static: une seule variable pour tous les objets créés (qqes soit le nombre d’objets)
+
+        //static protected nsSupportDessin.ISupportDessin? _SupportDessin; // Propriété  qui contient le support pour dessiner
+
+        //static public nsSupportDessin.ISupportDessin? SupportDessin// Accesseurs W
+
+        //{
+
+        //    set { _SupportDessin = value; }
+
+        
+
+        #endregion
+        //---------------------------------
+
+        // Dessin du Point
+
+        override internal void Dessine()
+
+        {
+
+            Console.WriteLine($"--- clsPoint.Dessine(X={X} Y={Y} \"{Nom}\")");
+
+
+
+            //// Si SupportDessin non défini (null), pas de traitement
+
+            //if (_SupportDessin != null)
+
+            //{
+
+            //  _ = _SupportDessin.Couleur_Selectionne(Couleur.R, Couleur.G, Couleur.B);
+
+            //  _ = _SupportDessin.Ligne_Trace(X, Y, X, Y);
+
+            //}
+
+
+
+            // Si SupportDessin null, pas de traitement
+
+            //_ = _SupportDessin?.Couleur_Selectionne(Couleur.R, Couleur.G, Couleur.B);
+
+            //_ = _SupportDessin?.Ligne_Trace(X, Y, X, Y);
+
+
+
+            //// Si SupportDessin non de type interface ISupportDessin (null ou autre type), pas de traitement
+
+            //if (_SupportDessin is nsSupportDessin.ISupportDessin)
+
+            //{
+
+            //  _ = _SupportDessin.Couleur_Selectionne(Couleur.R, Couleur.G, Couleur.B);
+
+            //  _ = _SupportDessin.Ligne_Trace(X, Y, X, Y);
+
+            //}
+
+        }
+
+
+
+        //---------------------------------
+
+        public override string ToString()
+
+        {
+
+            return $"Point \"{Nom}\": X={X} Y={Y}";
+
+        }
+
+    } //class clsPoint
+
+}
