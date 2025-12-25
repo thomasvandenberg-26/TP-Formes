5d5fb8e683a4bc41333719bda59d6a8b207ff695
thomasvandenberg-26 <vandenbergthomas@hotmail.fr>
2025-12-25 14:51:53 +0100

    Nouveaux Support pour implémentation Graphique

---
+++ ConsoleProject/Support_WinForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProject
+{
+    internal class Support_WinForm : ISupportDessin
+    {
+        int ISupportDessin.Cercle_Trace(int AX_Centre, int AY_Centre, int ARayon)
+        {
+            throw new NotImplementedException();
+        }
+
+        int ISupportDessin.Couleur_Selectionne(byte ARouge, byte AVert, byte ABleu)
+        {
+            throw new NotImplementedException();
+        }
+
+        int ISupportDessin.Ligne_Trace(int AX_Debut, int AY_Debut, int AX_Fin, int AY_Fin)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
