6f299e53455e34cb4a65e2d67e46b64417ca8dc3
thomasvandenberg-26 <vandenbergthomas@hotmail.fr>
2025-12-26 15:27:19 +0100

    Log WPF

---
+++ TP_Formes_WPF/TP_Formes_WPF/EventViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Formes_WPF
+{
+    public class EventViewModel
+    {
+        public DateTime Time { get; set; }
+        public string Type { get; set; }
+        public string Message { get; set; }
+
+        public override string ToString()
+            => $"[{Time:HH:mm:ss}] {Type} - {Message}";
+
+    };
+}
--- TP_Formes_WPF/TP_Formes_WPF/MainWindow.xaml.cs
+++ TP_Formes_WPF/TP_Formes_WPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using System.Windows;
+using System.Drawing; 
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -8,7 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using nsFigures;
 namespace TP_Formes_WPF
 {
     /// <summary>
@@ -19,6 +20,38 @@ namespace TP_Formes_WPF
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += (_, __) => Dessiner();
+            OpenLogWindow();
+        }
+
+        private void Dessiner()
+        {
+            MainCanvas.Children.Clear();
+
+            clsFigures.SupportDessin = new SupportWPF(MainCanvas);
+
+            var dessin = new Dessin("WPF", 1.0f);
+
+            dessin.Ajouter_Figure(new clsRectangle(
+               new System.Drawing.Point(50, 50),
+                System.Drawing.Color.Orange,
+                100,
+                80,"R1"
+            ));
+
+            dessin.Ajouter_Figure(new clsCube(
+                new System.Drawing.Point(200, 60),
+               System.Drawing.Color.Orange,
+                "C1",
+                60
+            ));
+
+            dessin.DessinerFigures();
+        }
+        private void OpenLogWindow()
+        {
+            var win = new LogEventsWindow();
+            win.Show();
         }
     }
 }
